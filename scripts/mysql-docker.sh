36e33d27f675eeebf345132dc282cb56304c6a11
songzhibin97 <718428482@qq.com>
2023-08-06 17:50:21 +0800

    Update mysql-docker.sh

--- scripts/mysql-docker.sh
+++ scripts/mysql-docker.sh
@@ -6,4 +6,4 @@ docker rm -f storage-lock-mysql
 # 启动MySQL实例，默认的用户名为root，密码为123456，监听在3306端口
 docker run -itd --name storage-lock-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=UeGqAm8CxYGldMDLoNNt mysql:5.7
 
-export STORAGE_LOCK_MYSQL_DSN="root:UeGqAm8CxYGldMDLoNNt@tcp(192.168.128.206:3306)/storage_lock_test"
+export STORAGE_LOCK_MYSQL_DSN="root:UeGqAm8CxYGldMDLoNNt@tcp(127.0.0.1:3306)/storage_lock_test"
